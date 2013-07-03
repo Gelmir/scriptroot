d6f56294cf34c7df3d7e29186f2f9c330768a734
Nick Tiskov <daymansmail@gmail.com>
2013-07-03 18:09:48 +0400

    Fix libtorrent 0.16.10 compilation with Boost 1.54

--- libtorrent/msvc64.bat
+++ libtorrent/msvc64.bat
@@ -31,6 +31,8 @@ IF NOT DEFINED RC (
   "C:\Program Files\7-Zip\7z.exe" x T:\_compressed_sources\libtorrent-0.16.10.7z -o%SOURCEROOT%\libtorrent
   patch --binary -p1 -Nfi %SCRIPTROOT%\libtorrent\patches\export_fix.patch
   IF ERRORLEVEL 1 GOTO FAIL
+  patch --binary -p3 -Nfi %SCRIPTROOT%\libtorrent\patches\boost_1_54_fix.patch
+  IF ERRORLEVEL 1 GOTO FAIL
   REM IF NOT DEFINED NO_TAINT (
   REM )
 ) ELSE (
