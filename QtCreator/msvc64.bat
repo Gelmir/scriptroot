678f39f8b2778f491681635b22d215df0a566fa5
Nick Tiskov <daymansmail@gmail.com>
2013-08-05 22:46:26 +0400

    Revert "Build Qt Creator against  Qt5"
    This reverts commit fc1c4824b6b964c491c035c609a17754dc28d28a.

--- QtCreator/msvc64.bat
+++ QtCreator/msvc64.bat
@@ -24,11 +24,11 @@ IF EXIST %BUILDROOT%\QtCreator RD /S /Q %BUILDROOT%\QtCreator
 CALL %SCRIPTROOT%\virgin.bat backup
 SET CWD=%CD%
 CALL "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin\amd64\vcvars64.bat"
-SET "PATH=%BUILDROOT%\Qt\Qt5_x64_full\bin;C:\_\Python27;C:\Program Files\7-Zip;%BUILDROOT%\jom;%PATH%"
+SET "PATH=%BUILDROOT%\Qt\Qt64\bin;T:\Python27;C:\Program Files\7-Zip;%BUILDROOT%\jom;%PATH%"
 IF EXIST %SOURCEROOT%\Qt RD /S /Q %SOURCEROOT%\Qt
 MD %SOURCEROOT%\Qt
 CD %SOURCEROOT%\Qt
-"C:\Program Files\7-Zip\7z.exe" x %ARCHIVES%\QT-5.1.0.7z -o%SOURCEROOT%\Qt qtbase/
+"C:\Program Files\7-Zip\7z.exe" x T:\_compressed_sources\QT-4.8.5.7z -o%SOURCEROOT%\Qt
 IF EXIST %SOURCEROOT%\QtCreator RD /S /Q %SOURCEROOT%\QtCreator
 MD %SOURCEROOT%\QtCreator
 CD %SOURCEROOT%\QtCreator
@@ -43,7 +43,7 @@ REM FOR %%X IN (%DISABLE_PLUGINS%) DO (
 REM )
 MD qtcb
 CD qtcb
-qmake -config release -r ../qtcreator.pro "QT_PRIVATE_HEADERS = C:/_/sources/Qt/include " "CONFIG += warn_off ltcg mmx sse sse2" "CONFIG -= 3dnow"
+qmake -config release -r ../qtcreator.pro "QT_PRIVATE_HEADERS = T:/sources/Qt/include " "CONFIG += warn_off ltcg mmx sse sse2" "CONFIG -= 3dnow"
 IF ERRORLEVEL 1 GOTO FAIL
 jom -j4
 IF ERRORLEVEL 1 GOTO FAIL
@@ -56,6 +56,9 @@ MOVE /Y %SOURCEROOT%\QtCreator\scripts\deployqt.py.%SEDEXT% %SOURCEROOT%\QtCreat
 :: Sqlite is built-in in QtSql4 and not built as a plugin, skip
 sed -b -e "/^[[:space:]]\+plugins = /s|\(.*\), 'sqldrivers'|\1|" < ..\scripts\deployqt.py > ..\scripts\deployqt.py.%SEDEXT%
 MOVE /Y %SOURCEROOT%\QtCreator\scripts\deployqt.py.%SEDEXT% %SOURCEROOT%\QtCreator\scripts\deployqt.py
+:: Translations are not needed
+REM sed -b -e "/^[[:space:]]\+copy_translations/s|\(.*\)|#\1|" < ..\scripts\deployqt.py > ..\scripts\deployqt.py.%SEDEXT%
+REM MOVE /Y %SOURCEROOT%\QtCreator\scripts\deployqt.py.%SEDEXT% %SOURCEROOT%\QtCreator\scripts\deployqt.py
 jom -j1 bindist
 IF ERRORLEVEL 1 GOTO FAIL
 jom -j1 install_docs
@@ -67,15 +70,15 @@ CALL %SCRIPTROOT%\virgin.bat backup
 MD qtcb
 CD qtcb
 CALL "C:\Program Files (x86)\Microsoft Visual Studio 10.0\VC\bin\vcvars32.bat"
-SET "PATH=%BUILDROOT%\Qt\Qt5_x64_full\bin;C:\_\Python27;C:\Program Files\7-Zip;%BUILDROOT%\jom;%PATH%"
+SET "PATH=%BUILDROOT%\Qt\Qt64\bin;T:\Python27;C:\Program Files\7-Zip;%BUILDROOT%\jom;%PATH%"
 :: Prepare 32-bit mkspecs
 IF EXIST %SOURCEROOT%\mkspecs RD /S /Q %SOURCEROOT%\mkspecs
-"C:\Program Files\7-Zip\7z.exe" x -o%SOURCEROOT% %ARCHIVES%\QT-4.8.5.7z qtbase/mkspecs
-patch --binary -p1 -Nsfi %SCRIPTROOT%\Qt\patches\msvc_Qt5.diff -d %SOURCEROOT%\
+"C:\Program Files\7-Zip\7z.exe" x -o%SOURCEROOT% T:\_compressed_sources\QT-4.8.5.7z mkspecs
+patch --binary -p1 -Nsfi %SCRIPTROOT%\Qt\patches\msvc_R.diff -d %SOURCEROOT%\
 :: ignore errors here
 :: IF ERRORLEVEL 1 GOTO FAIL
 SET "QMAKESPEC=%SOURCEROOT%\mkspecs\win32-msvc2010"
-qmake -config release -r ../qtcreator.pro "QT_PRIVATE_HEADERS = C:/_/sources/Qt/include " "CONFIG += warn_off msvc_mp ltcg mmx sse sse2" "CONFIG -= 3dnow"
+qmake -config release -r ../qtcreator.pro "QT_PRIVATE_HEADERS = T:/sources/Qt/include " "CONFIG += warn_off msvc_mp ltcg mmx sse sse2" "CONFIG -= 3dnow"
 IF ERRORLEVEL 1 GOTO FAIL
 CD .\src\libs\qtcreatorcdbext
 :: Only building i686 cdb helper
@@ -87,13 +90,13 @@ IF ERRORLEVEL 1 GOTO FAIL
 FOR %%X IN (assistant qt designer linguist qmake qml) DO (
     IF EXIST %INSTALL_ROOT%\share\doc\%%X RD /S /Q %INSTALL_ROOT%\share\doc\%%X
     MD %INSTALL_ROOT%\share\doc\%%X
-    COPY /Y %BUILDROOT%\Qt\Qt5_x64_full\doc\qch\%%X.qch %INSTALL_ROOT%\share\doc\%%X\
+    COPY /Y %BUILDROOT%\Qt\Qt64\doc\qch\%%X.qch %INSTALL_ROOT%\share\doc\%%X\
 )
 :: Copy SSL libs
 XCOPY /Y /Q %BUILDROOT%\OpenSSL\OpenSSL64\bin\*.dll %INSTALL_ROOT%\bin\
 :: Copy whatever 'nmake bindist' forgot to copy
 FOR %%X IN (bearer codecs phonon_backend) DO (
-	XCOPY /E /Y /Q /I %BUILDROOT%\Qt\Qt5_x64_full\plugins\%%X %INSTALL_ROOT%\bin\%%X
+	XCOPY /E /Y /Q /I %BUILDROOT%\Qt\Qt64\plugins\%%X %INSTALL_ROOT%\bin\%%X
 )
 :: Purge .lib files
 FOR /R %INSTALL_ROOT% %%X IN (*.lib) DO DEL /Q %%X
