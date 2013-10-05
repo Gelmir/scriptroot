cd293edeca54260c4df856b3ed85f31112981ca9
Nick Tiskov <daymansmail@gmail.com>
2013-10-06 01:45:14 +0400

    Update full-blown Qt5 build script:
     * Use developer build (for private headers)
     * Use accessibility module
     * Fix missing libraries after installation

--- Qt/msvc64_Qt5_full.bat
+++ Qt/msvc64_Qt5_full.bat
@@ -28,18 +28,21 @@ IF ERRORLEVEL 1 GOTO FAIL
 patch --binary -p1 -Nsfi %SCRIPTROOT%\Qt\patches\5.1.1_fixes.patch
 IF ERRORLEVEL 1 GOTO FAIL
 SET "PATH=%BUILDROOT%\icu\icu64\bin64;C:\_\ruby\bin;%SOURCEROOT%\qt\gnuwin32\bin;%INST_DIR%\bin;%PATH%"
-CALL configure.bat -release -shared -opensource -confirm-license -platform win32-msvc2010 -ltcg -no-accessibility -no-sql-mysql -no-sql-psql -no-sql-oci -no-sql-odbc -no-sql-tds -no-sql-db2 -qt-sql-sqlite -no-sql-sqlite2 -no-sql-ibase -opengl desktop -no-angle -no-openvg -qt-zlib -qt-libpng -qt-libjpeg -icu -qt-pcre -qt-freetype -no-vcproj -no-incredibuild-xge -plugin-manifests -process -no-mp -rtti -sse2 -no-sse3 -no-ssse3 -no-sse4.1 -no-sse4.2 -no-avx -no-avx2 -openssl -no-dbus -audio-backend -qml-debug -no-style-windowsmobile -no-style-windowsce -qt-style-fusion -qt-style-windows -qt-style-windowsxp -qt-style-windowsvista -no-native-gestures -no-directwrite -qmake -nomake examples -skip qtwebkit-examples -no-warnings-are-errors -I %BUILDROOT%\OpenSSL\OpenSSL64\include -I %BUILDROOT%\icu\icu64\include -L %BUILDROOT%\OpenSSL\OpenSSL64\lib -L %BUILDROOT%\icu\icu64\lib64 -prefix %INST_DIR%
+CALL configure.bat -developer-build -release -shared -opensource -confirm-license -platform win32-msvc2010 -ltcg -accessibility -no-sql-mysql -no-sql-psql -no-sql-oci -no-sql-odbc -no-sql-tds -no-sql-db2 -qt-sql-sqlite -no-sql-sqlite2 -no-sql-ibase -opengl desktop -no-angle -no-openvg -qt-zlib -qt-libpng -qt-libjpeg -icu -qt-pcre -qt-freetype -no-vcproj -no-incredibuild-xge -plugin-manifests -process -no-mp -rtti -sse2 -no-sse3 -no-ssse3 -no-sse4.1 -no-sse4.2 -no-avx -no-avx2 -openssl -no-dbus -audio-backend -qml-debug -no-style-windowsmobile -no-style-windowsce -qt-style-fusion -qt-style-windows -qt-style-windowsxp -qt-style-windowsvista -no-native-gestures -no-directwrite -qmake -nomake examples -nomake tests -skip qtwebkit-examples -no-warnings-are-errors -I %BUILDROOT%\OpenSSL\OpenSSL64\include -I %BUILDROOT%\icu\icu64\include -L %BUILDROOT%\OpenSSL\OpenSSL64\lib -L %BUILDROOT%\icu\icu64\lib64 -prefix %INST_DIR%
 IF ERRORLEVEL 1 GOTO FAIL
 jom -j4
 IF ERRORLEVEL 1 GOTO FAIL
 jom -j1 install
 IF ERRORLEVEL 1 GOTO FAIL
 :: Perform installation of missing parts
-jom -j1 module-qtwebkit-install_subtargets
+jom -j1 module-qttranslations-install_subtargets
 IF ERRORLEVEL 1 GOTO FAIL
-jom -j1 module-qttools-install_subtargets
+jom -j1 module-qtmultimedia-install_subtargets
 IF ERRORLEVEL 1 GOTO FAIL
-jom -j1 qch_docs
+jom -j1 module-qtdeclarative-install_subtargets
+IF ERRORLEVEL 1 GOTO FAIL
+:: Required for qch_docs
+jom -j1 docs
 IF ERRORLEVEL 1 GOTO FAIL
 jom -j1 install_qch_docs
 IF ERRORLEVEL 1 GOTO FAIL
