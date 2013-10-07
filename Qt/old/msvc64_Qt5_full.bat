2d9b2891fa6797a26c237ce88b8835f3ccf60603
Nick Tiskov <daymansmail@gmail.com>
2013-10-08 01:46:08 +0400

    Drop full-blown Qt5 build. Can't build my shiny Qt Creator 3.0 anyway.

--- Qt/old/msvc64_Qt5_full.bat
+++ Qt/old/msvc64_Qt5_full.bat
@@ -27,6 +27,8 @@ patch --binary -p1 -Nsfi %SCRIPTROOT%\Qt\patches\msvc64_Qt5.diff
 IF ERRORLEVEL 1 GOTO FAIL
 patch --binary -p1 -Nsfi %SCRIPTROOT%\Qt\patches\5.1.1_fixes.patch
 IF ERRORLEVEL 1 GOTO FAIL
+patch --binary -p1 -Nsfi %SCRIPTROOT%\Qt\patches\5.1.1-webkit-msvc2012.patch
+IF ERRORLEVEL 1 GOTO FAIL
 SET "PATH=%BUILDROOT%\icu\icu64\bin64;C:\_\ruby\bin;%SOURCEROOT%\qt\gnuwin32\bin;%INST_DIR%\bin;%PATH%"
 CALL configure.bat -developer-build -release -shared -opensource -confirm-license -platform win32-msvc2012 -ltcg -accessibility -no-sql-mysql -no-sql-psql -no-sql-oci -no-sql-odbc -no-sql-tds -no-sql-db2 -qt-sql-sqlite -no-sql-sqlite2 -no-sql-ibase -opengl desktop -no-angle -no-openvg -qt-zlib -qt-libpng -qt-libjpeg -icu -qt-pcre -qt-freetype -no-vcproj -no-incredibuild-xge -plugin-manifests -process -no-mp -rtti -sse2 -no-sse3 -no-ssse3 -no-sse4.1 -no-sse4.2 -no-avx -no-avx2 -openssl -no-dbus -audio-backend -qml-debug -no-style-windowsmobile -no-style-windowsce -qt-style-fusion -qt-style-windows -qt-style-windowsxp -qt-style-windowsvista -no-native-gestures -no-directwrite -qmake -nomake examples -nomake tests -skip qtwebkit-examples -no-warnings-are-errors -I %BUILDROOT%\OpenSSL\OpenSSL64\include -I %BUILDROOT%\icu\icu64\include -L %BUILDROOT%\OpenSSL\OpenSSL64\lib -L %BUILDROOT%\icu\icu64\lib64 -prefix %INST_DIR%
 IF ERRORLEVEL 1 GOTO FAIL
