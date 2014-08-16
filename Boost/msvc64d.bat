ae49825533a451adfb8944a56682264fa57d655e
Nick Tiskov <daymansmail@gmail.com>
2014-08-17 02:32:54 +0400

    Update to Boost 1.56

--- Boost/msvc64d.bat
+++ Boost/msvc64d.bat
@@ -21,7 +21,7 @@ CALL "C:\Program Files (x86)\Microsoft Visual Studio 11.0\VC\bin\x86_amd64\vcvar
 IF EXIST %SOURCEROOT%\Boost RD /S /Q %SOURCEROOT%\Boost
 MD %SOURCEROOT%\Boost
 CD %SOURCEROOT%\Boost
-"C:\Program Files\7-Zip\7z.exe" x %ARCHIVES%\boost-1.55.7z -o%SOURCEROOT%\Boost
+"C:\Program Files\7-Zip\7z.exe" x %ARCHIVES%\boost-1.56.7z -o%SOURCEROOT%\Boost
 SET "PATH=%BUILDROOT%\Boost\bjam64\bin;%PATH%"
 @ECHO OFF
 bjam -j4 -q --with-system --toolset=msvc --layout=system --prefix=%INST_DIR% link=shared runtime-link=shared variant=debug debug-symbols=on threading=multi address-model=64 host-os=windows target-os=windows embed-manifest=on architecture=x86 warnings=off warnings-as-errors=off "cflags=/Zi /favor:blend" "linkflags=/NOLOGO /DEBUG /INCREMENTAL:NO" install
