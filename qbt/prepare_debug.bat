3cdf36ee4bdf84e8117be08a779bb6b1fe0b3f2c
Nick Tiskov <daymansmail@gmail.com>
2014-12-21 00:50:50 +0300

    Modify and rename ename qBt debug prepare script.

--- qbt/prepare_debug.bat
+++ qbt/prepare_debug.bat
@@ -3,14 +3,19 @@ GOTO BEGIN
 :CLEANUP
 SET QBT_ROOT=
 SET QT5=
+SET GCC=
 GOTO END
 :FAIL
 ECHO Building failed, leaving source tree as is and dumping custom env vars
 IF DEFINED QBT_ROOT ECHO QBT_ROOT = %QBT_ROOT%
+IF DEFINED QT5 ECHO QT5 = %QT5%
+IF DEFINED GCC ECHO GCC = %GCC%
 SET QBT_ROOT=
 SET QT5=
+SET GCC=
 GOTO END
 :BEGIN
+Setlocal EnableDelayedExpansion
 SET "QBT_ROOT=C:\_\WORK\qBittorrent"
 IF EXIST %QBT_ROOT%\qbittorrent.pro.user COPY /Y %QBT_ROOT%\qbittorrent.pro.user %TEMP%\qbittorrent.pro.user
 IF EXIST %QBT_ROOT% RD /S /Q %QBT_ROOT%
@@ -18,14 +23,21 @@ IF EXIST %QBT_ROOT%\..\build-qbittorrent RD /S /Q %QBT_ROOT%\..\build-qbittorren
 MD %QBT_ROOT%
 XCOPY /E /Y /Q /I C:\Users\Dayman\Documents\GitHub\qBittorrent %QBT_ROOT%\
 IF EXIST %TEMP%\qbittorrent.pro.user COPY /Y %TEMP%\qbittorrent.pro.user %QBT_ROOT%\qbittorrent.pro.user
-IF ERRORLEVEL 1 GOTO FAIL
+REM IF ERRORLEVEL 1 GOTO FAIL
 "C:\Program Files\7-Zip\7z.exe" x %ARCHIVES%\GeoIP.7z -o%QBT_ROOT%\src\geoip\
 IF ERRORLEVEL 1 GOTO FAIL
+IF NOT DEFINED GCC GOTO MSVC
+GOTO MINGW
+:MSVC
 IF DEFINED QT5 (
   patch --binary -d %QBT_ROOT% -p1 -Nsfi %SCRIPTROOT%\qbt\patches\msvc64d_Qt5.patch
 ) ELSE (
   patch --binary -d %QBT_ROOT% -p1 -Nsfi %SCRIPTROOT%\qbt\patches\msvc64d.patch
 )
+IF ERRORLEVEL 1 GOTO FAIL
+GOTO CLEANUP
+:MINGW
+patch --binary -d %QBT_ROOT% -p1 -Nsfi %SCRIPTROOT%\qbt\patches\gcc64.patch
 IF ERRORLEVEL 1 GOTO FAIL
 GOTO CLEANUP
 :END
