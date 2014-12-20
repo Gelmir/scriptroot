86aa48b75069bd62730f179a7ef8b9fbc3795f47
Nick Tiskov <daymansmail@gmail.com>
2014-12-21 01:26:18 +0300

    Don't apply patches for msvc Qt4 version

--- Qt/msvc64_Qt4d_qbt.bat
+++ Qt/msvc64_Qt4d_qbt.bat
@@ -22,8 +22,6 @@ IF EXIST %SOURCEROOT%\Qt RD /S /Q %SOURCEROOT%\Qt
 MD %SOURCEROOT%\Qt
 CD %SOURCEROOT%\Qt
 "C:\Program Files\7-Zip\7z.exe" x %ARCHIVES%\QT-4.8.6.7z -o%SOURCEROOT%\Qt
-patch --binary -p1 -Nfi %SCRIPTROOT%\Qt\patches\msvc64_Qt4.diff
-IF ERRORLEVEL 1 GOTO FAIL
 SET "PATH=%BUILDROOT%\jom;%PATH%"
 .\configure.exe -debug -shared -opensource -confirm-license -platform win32-msvc2012 -arch windows -no-ltcg -no-fast -exceptions -no-accessibility -stl -no-sql-mysql -no-sql-psql -no-sql-oci -no-sql-odbc -no-sql-tds -no-sql-db2 -qt-sql-sqlite -no-sql-sqlite2 -no-sql-ibase -no-qt3support -no-opengl -no-openvg -graphicssystem raster -qt-zlib -qt-libpng -qt-libmng -qt-libtiff -qt-libjpeg -no-dsp -no-vcproj -no-incredibuild-xge -plugin-manifests -process -no-mp -rtti -no-3dnow -mmx -sse -sse2 -openssl -no-dbus -no-phonon -no-phonon-backend -no-multimedia -no-audio-backend -no-webkit -no-script -no-scripttools -no-declarative -no-declarative-debug -no-style-s60 -no-style-windowsmobile -no-style-windowsce -no-style-cde -no-style-motif -qt-style-cleanlooks -qt-style-plastique -qt-style-windows -qt-style-windowsxp -qt-style-windowsvista -no-native-gestures -no-directwrite -qmake -nomake examples -nomake demos -I %BUILDROOT%\OpenSSL\OpenSSL64d\include -L %BUILDROOT%\OpenSSL\OpenSSL64d\lib -prefix %INST_DIR%
 IF ERRORLEVEL 1 GOTO FAIL
