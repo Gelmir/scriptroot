f3ee00075b0ef36e0f054e8f584e82268551ea03
Nick Tiskov <daymansmail@gmail.com>
2016-01-24 15:50:56 +0300

    Update to Boost 1.60

--- Boost/msvc64.bat
+++ Boost/msvc64.bat
@@ -21,14 +21,14 @@ CALL "C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\bin\x86_amd64\vcvar
 IF EXIST %SOURCEROOT%\Boost RD /S /Q %SOURCEROOT%\Boost
 MD %SOURCEROOT%\Boost
 CD %SOURCEROOT%\Boost
-"C:\Program Files\7-Zip\7z.exe" x %ARCHIVES%\boost-1.55.7z -o%SOURCEROOT%\Boost
+"C:\Program Files\7-Zip\7z.exe" x %ARCHIVES%\boost-1.60.7z -o%SOURCEROOT%\Boost
 IF EXIST %BUILDROOT%\Boost\bjam64 RD /S /Q %BUILDROOT%\Boost\bjam64
-CD .\tools\build\v2
+CD .\tools\build
 CALL .\bootstrap.bat
 IF ERRORLEVEL 1 GOTO FAIL
 .\b2.exe --toolset=msvc architecture=x86 address-model=64 --prefix=%BUILDROOT%\Boost\bjam64 link=shared runtime-link=shared variant=release debug-symbols=off warnings=off warnings-as-errors=off inlining=full optimization=speed "cflags=/O2 /GL /favor:AMD64" "linkflags=/NOLOGO /OPT:REF /OPT:ICF=5 /LTCG" install
 IF ERRORLEVEL 1 GOTO FAIL
-CD ..\..\..\
+CD ..\..\
 SET "PATH=%BUILDROOT%\Boost\bjam64\bin;%PATH%"
 @ECHO OFF
 bjam -j8 -q --with-system --with-date_time --toolset=msvc --layout=system --prefix=%INST_DIR% link=shared runtime-link=shared variant=release debug-symbols=off threading=multi address-model=64 host-os=windows target-os=windows embed-manifest=on architecture=x86 warnings=off warnings-as-errors=off inlining=full optimization=speed "cflags=/O2 /GL /favor:blend" "linkflags=/NOLOGO /OPT:REF /OPT:ICF=5 /LTCG" install
