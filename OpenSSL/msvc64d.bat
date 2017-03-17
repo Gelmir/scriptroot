55510fb65899f3a167d60a1b4f512f84ccbe941a
Nick Tiskov <daymansmail@gmail.com>
2017-03-17 21:26:02 +0300

    Use OpenSSL 1.0.2k

--- OpenSSL/msvc64d.bat
+++ OpenSSL/msvc64d.bat
@@ -30,7 +30,7 @@ CALL "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\bin\x86_amd64\vcvar
 IF EXIST %SOURCEROOT%\OpenSSL RD /S /Q %SOURCEROOT%\OpenSSL
 MD %SOURCEROOT%\OpenSSL
 CD %SOURCEROOT%\OpenSSL
-"C:\Program Files\7-Zip\7z.exe" x %ARCHIVES%\OpenSSL-1.0.2j.7z -o%SOURCEROOT%\OpenSSL
+"C:\Program Files\7-Zip\7z.exe" x %ARCHIVES%\OpenSSL-1.0.2k.7z -o%SOURCEROOT%\OpenSSL
 SET "CFLAGS=/favor:blend /Od /FS /Y- /MP /MDd /W3 /nologo"
 SET "LDFLAGS=/NOLOGO /DEBUG /INCREMENTAL:NO /subsystem:console"
 SET "MLFLAGS=/NOLOGO /DEBUG /INCREMENTAL:NO /subsystem:console /DLL"
