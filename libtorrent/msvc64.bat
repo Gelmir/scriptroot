7cf1ba1f49a8389207ae16b502ff3ecca68113d8
Nick Tiskov <daymansmail@gmail.com>
2017-07-27 23:15:28 +0300

    Use libtorrent 1.1.4

--- libtorrent/msvc64.bat
+++ libtorrent/msvc64.bat
@@ -24,7 +24,7 @@ CALL "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary
 IF EXIST %SOURCEROOT%\libtorrent RD /S /Q %SOURCEROOT%\libtorrent
 MD %SOURCEROOT%\libtorrent
 CD %SOURCEROOT%\libtorrent
-"C:\Program Files\7-Zip\7z.exe" x %ARCHIVES%\libtorrent-1.1.3.7z -o%SOURCEROOT%\libtorrent
+"C:\Program Files\7-Zip\7z.exe" x %ARCHIVES%\libtorrent-1.1.4.7z -o%SOURCEROOT%\libtorrent
 SET "PATH=%BUILDROOT%\Boost\bjam64\bin;%PATH%"
 ECHO using msvc : : "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.10.25017\bin\HostX64\x64\cl.exe" ; > project-config.jam
 IF DEFINED LOG (
