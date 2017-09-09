85b744214c56d68c0129920d926d801c424d8b16
Nick Tiskov <daymansmail@gmail.com>
2017-09-09 14:08:42 +0300

    Revert "Fix Boost and libtorrent with MSVC 2017"
    This reverts commit 47f7931392a93d47f4b73cfa69f29f3458f86a9b.

--- Boost/msvc64d.bat
+++ Boost/msvc64d.bat
@@ -23,7 +23,6 @@ MD %SOURCEROOT%\Boost
 CD %SOURCEROOT%\Boost
 "C:\Program Files\7-Zip\7z.exe" x %ARCHIVES%\boost-1.65.1.7z -o%SOURCEROOT%\Boost
 SET "PATH=%BUILDROOT%\Boost\bjam64\bin;%PATH%"
-ECHO using msvc : : "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.10.25017\bin\HostX64\x64\cl.exe" ; > project-config.jam
 @ECHO OFF
 bjam -j8 -q --with-system --with-chrono --with-random --with-date_time --toolset=msvc --layout=system --prefix=%INST_DIR% link=shared runtime-link=shared variant=debug debug-symbols=on threading=multi address-model=64 host-os=windows target-os=windows embed-manifest=on architecture=x86 warnings=off warnings-as-errors=off "cflags=/Zi /FS /favor:blend" "linkflags=/NOLOGO /DEBUG /INCREMENTAL:NO" install
 IF ERRORLEVEL 1 GOTO FAIL
--- libtorrent/msvc64_0.bat
+++ libtorrent/msvc64_0.bat
@@ -26,7 +26,6 @@ MD %SOURCEROOT%\libtorrent
 CD %SOURCEROOT%\libtorrent
 "C:\Program Files\7-Zip\7z.exe" x %ARCHIVES%\libtorrent-1.0.11.7z -o%SOURCEROOT%\libtorrent
 SET "PATH=%BUILDROOT%\Boost\bjam64\bin;%PATH%"
-ECHO using msvc : : "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.10.25017\bin\HostX64\x64\cl.exe" ; > project-config.jam
 IF DEFINED LOG SET "LOG=disk-stats=on logging=default"
 bjam -j8 -q --toolset=msvc --prefix=%INST_DIR% %LOG% boost=system boost-link=shared link=shared runtime-link=shared variant=release debug-symbols=off resolve-countries=on full-stats=on export-extra=on ipv6=on character-set=unicode geoip=static encryption=openssl windows-version=vista threading=multi address-model=64 host-os=windows target-os=windows embed-manifest=on architecture=x86 warnings=off warnings-as-errors=off inlining=full optimization=speed "cflags=/O2 /GL /favor:blend" "linkflags=/NOLOGO /OPT:REF /OPT:ICF=5 /LTCG" "include=%BUILDROOT%\OpenSSL\OpenSSL64\include" "include=%BUILDROOT%\Boost\Boost64\include" "library-path=%BUILDROOT%\OpenSSL\OpenSSL64\lib" "library-path=%BUILDROOT%\Boost\Boost64\lib" "define=BOOST_ALL_NO_LIB" install
 IF ERRORLEVEL 1 GOTO FAIL
