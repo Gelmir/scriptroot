53711a5b6193485c328e59465a8613be1493b1e5
Nick Tiskov <daymansmail@gmail.com>
2017-11-10 01:19:26 +0300

    Add PS script for checking command failures and ending the build

---
+++ End-Build.ps1
@@ -0,0 +1,18 @@
+﻿function End-Build ([ValidateNotNullOrEmpty()][bool]$result = $true) {
+    Set-Location -Path "$cwd"
+
+    if (-not $result) {
+        Write-Warning "`r`n`r`nBuild failed, leaving source tree as is"
+    } else {
+        Remove-Item -Path "$env:SOURCEROOT\$libname" -Force -Recurse
+    }
+
+    Env-Restore $envBackup
+    exit
+}
+
+function Check-ReturnCode ([ValidateNotNullOrEmpty()][bool]$result) {
+    if (-not $result) {
+        End-Build $result
+    }
+}
